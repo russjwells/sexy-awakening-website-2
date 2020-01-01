b42824a508f942f8547dc55f001ee28984fd9b3b
Russ Wells <russ@russdigital.com>
2019-12-31 18:38:27 -0800

    copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,19 +13,21 @@ const IndexPage = () => (
     <h2>Swipe With Intention</h2>
     <p>Swipe right for sex, up for romance, left for friendship, and down to pass. If someone swipes you with the same intention you are connected and can begin to chat with <b>mutual understanding</b>.</p>
     <h2>About Us</h2>
-    <p>We believe that relationships are a <b>transformational experience</b> and we exist in order to facilitate <b>evolutionary connections</b>. We encourage honest upfront communication and radical self-expression on our platform. Sexy Awakening is a place for and to connect with beings of <b>higher consciousness</b>. This is a sex positive and inclusive space held for expression, growth, play and connection. On Sexy Awakening there is space for you. We understand sexuality to be fluid, gender as nonbinary, and that everyone is sacred. Enjoy yourselves.</p>
+    <p>We believe that relationships are a <b>transformational experience</b> and we exist in order to facilitate <b>spiritual evolution</b>. We encourage honest upfront communication and radical self-expression on our platform. Sexy Awakening is a place for and to connect with beings of <b>higher consciousness</b>. This is a sex positive and inclusive space held for expression, growth, play and connection. On Sexy Awakening there is space for you. We understand sexuality to be fluid, gender as nonbinary, and that everyone is sacred. Enjoy yourselves.</p>
     <h2>Membership</h2>
-    <p>Sexy Awakening is <b>free to join</b>. Full memberships will be available for $11 per month. You must be 18 or older to create an account.</p>
+    <p>Sexy Awakening is <b>free to join</b>, but designed to be paid for. Full memberships will be available for $11 per month.</p>
+    <p>You must be 18 or older to create an account.</p>
     <h2>How to Join</h2>
-    <p>We are in the approval process for the app stores. For now, use these options:</p>
-    <p><b>Android: </b><a href="https://play.google.com/store/apps/details?id=com.DigitalHyperspace.SexyAwakening">Play Store</a> <b>iOS: </b>Coming Soon <b>Web: </b>Coming Soon</p>
+    <p><b>Android: </b><a href="https://play.google.com/store/apps/details?id=com.DigitalHyperspace.SexyAwakening">Play Store</a></p>
+    <p><b>iOS: </b>Coming Soon </p>
+    <p> <b>Web: </b>Coming Soon </p>
     {/*<p>iPhone users download the app <a href="https://testflight.apple.com/join/A52QNb4b">here</a>. Android users can download the App in the <a href="https://play.google.com/store/apps/details?id=com.DigitalHyperspace.SexyAwakening">Play Store.</a></p>*/}
     <h2>Conscious Culture</h2>
     <p>Sexy Awakening is a transformational space.</p>
     <p>Sexy Awakening encourages a culture of immediacy, sexuality, play, exploration, intuition, and inspiration that is grounded in a deeply rooted sense of autonomy, respect, and soverignty.</p>
     <p>We are creating a culture of consent. In a transformational space it is important to respect each individual's boundaries, and to be able to hold your own.</p>
-    <h2>Support Us on Patreon</h2>
-    <p>If you'd like to support Sexy Awakening before paid memberships are live then you may <a href="https://www.patreon.com/sexyawakening">become our Patron</a> on Patreon. Sexy Awakening is independently created and funded.</p>
+    <h2>Support Us</h2>
+    <p>Sexy Awakening is independently created and funded. If you'd like to support Sexy Awakening before paid memberships are live then you may <a href="https://www.patreon.com/sexyawakening">become our Patron</a> on Patreon. </p>
     <h2>Coming Soon</h2>
     <p>We are adding more features to make Sexy Awakening even more activating, empowering, and safe. <Link to="/coming-soon/">See more</Link>.</p>
   </Layout>
