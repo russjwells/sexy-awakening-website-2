84889b0ca1c55ef1c9295005d92c3df9d8c25007
Russ Wells <russ@russdigital.com>
2020-01-13 13:25:44 -0800

    more terms and policy updates

--- src/pages/cancellation-policy.js
+++ src/pages/cancellation-policy.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const CancellationPolicy = () => (
   <Layout>
     <SEO title="CancellationPolicy" />
-    <div class="formatted-text"> <h2>Sexy Awakening Privacy Policy</h2>
+    <div class="formatted-text"> <h2>Sexy Awakening Cancellation Policy</h2>
       <p>You can cancel at any time during your subscription. When your paid membership ends your membership will become free and you will still be able to enjoy the free tier of Sexy Awakening.</p>
     </div>
     <Link to="/">Go back to the homepage</Link>
